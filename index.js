a556e3323b4c0467d2dd1198345248c36a360df2
pawan.singh <pawan.singh@paytm.com>
2016-07-05 22:41:12 +0530

    minor change

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = module.exports = express();
 app.use(express.bodyParser());
-var Db = require('mongodb').Db;
+//var Db = require('mongodb').Db;
 var router = express.Router();
 app.use(express.static(__dirname + '/'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 //require(__dirname + '/backend/contactus')(app);
-//require(__dirname + '/backend/users')(app);
-//require(__dirname + '/backend/product')(app);
-//require(__dirname + '/backend/slides')(app);
-//require(__dirname + '/backend/category')(app);
-//require(__dirname + '/backend/entity')(app);
+require(__dirname + '/backend/users')(app);
+require(__dirname + '/backend/product')(app);
+require(__dirname + '/backend/slides')(app);
+require(__dirname + '/backend/category')(app);
+require(__dirname + '/backend/entity')(app);
 
 app.use('/admin', function (req, res , next) {
     res.sendfile(__dirname + '/admin.html');
